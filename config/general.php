5dcdc441bb576fcb254d9706b9d0fe833397ccc6
brianhanson <brian@brianhanson.net>
2022-08-03 21:57:58 -0500

    WIP

---
+++ config/general.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\config\GeneralConfig;
+use craft\helpers\App;
+
+$isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
+$isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
+
+return GeneralConfig::create()
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
+    ->omitScriptNameInUrls()
+    // Enable Dev Mode on the dev environment (see https://craftcms.com/guides/what-dev-mode-does)
+    ->devMode($isDev)
+    // Only allow administrative changes on the dev environment
+    ->allowAdminChanges($isDev)
+    // Disallow robots everywhere except the production environment
+    ->disallowRobots(!$isProd)
+;
