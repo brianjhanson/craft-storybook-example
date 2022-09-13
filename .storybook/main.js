082addda53d4319509c7e99a8916eaa2953e55b9
Brian Hanson <brian@brianhanson.net>
2022-09-13 16:50:53 -0500

    Install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(json)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/server"
+}
